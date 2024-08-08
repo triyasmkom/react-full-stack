27dc0e70d4101843704e884241d8bd64449bcccc
triyasheviantosaputro <triyasvian@gmail.com>
2024-08-08 08:53:50 +0700

    initial code

---
+++ server/models/Posts.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes)=>{
+    const Post = sequelize.define("Post", {
+        title:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        postText:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        username:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        
+    })
+
+    return Post
+}
